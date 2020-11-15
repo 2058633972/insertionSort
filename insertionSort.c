38706f86d72978c64c15a1eca4ac81d59bb9221a
2058633972 <helloqinhaicheng@outlook.com>
2020-11-15 22:57:05 +0800

    改用指针来实现

--- insertionSort.c
+++ insertionSort.c
@@ -1,21 +1,20 @@
-#include"insertionsort.h"//头文件
-void insertionSort(int *a,int n){
-	/*先将数组a的前j个元素排好序，得出一个已经排好序的子序列a[0]~a[j-1],
-	 *再把a[j]插入到该子序列中合适的位置（该位置通过比较来确定），
-	 *这子序列就扩充了一个元素且仍然是排好序的，
-	 *以此类推不断扩充就能把a的全部元素排好序
-	 */
-	int i,j,key;
-	for(j=1;j<n;j++){
-		//子序列初始时只有一个元素a[0]
-		//for循环每循环一次子序列就扩充一个元素
-		key=a[j];
+#include"insertionsort.h"//头文件 
+void insertionSort(int *a,int *b){
+	//a,b分别是待排序列的首位指针
+	int i,j,key,*p,*q,*t,n=b-a;
+	for(j=1,p=q=a,p++,t=p;j<n;j++,q=p,p++,t=p){
+	//p用来标记子序列后边界之后的第一个元素
+		key=*p;
 		i=j-1;
-		while((i>=0)&&(a[i]>key)){
-			//找出插入位置
-			a[i+1]=a[i];
+		while((i>=0)&&(*q>key)){
+		//q用来指向排好序的子序列中每次进行比较的元素
+		//t用来指向想要新插入的的元素	
+			*t=*q;
+			t--;
 			i--;
+			if(q!=a)q--;
 		}
-		a[i+1]=key;
+		*t=key;
 	}
+
 }
--- test1.c
+++ test1.c
@@ -4,7 +4,7 @@
 #include"insertionSort.c"//实现头文件中声明的函数
 int main(int argc,char** argv){
 	int a[]={5,1,9,4,6,2,10,3,8,7},i;
-	insertionSort(a,10);//调用函数对数组a进行插入排序
+	insertionSort(a,a+10);//调用函数对数组a进行插入排序
 	for(i=0;i<10;i++)
 		printf("%d ",a[i]);//输出排序结果
 	printf("\n");
